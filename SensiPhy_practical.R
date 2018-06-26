7cc4701aeb1c1f3efd2b2dd5961e42324efb9749
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2018-06-26 14:31:36 +0100

    Plots

--- SensiPhy_practical.R
+++ SensiPhy_practical.R
@@ -4,17 +4,23 @@ library(ggplot2)
 library(devtools)
 devtools::install_github("paternogbc/sensiPhy")
 
+
+
+#1. tree_phylm: tree method for phylogenetic linear regression
+
+
 # Load data
 alien <- data
+
 # This analysis needs a multiphylo file:
-class(alien$phy) #Checking it's multiphylo
-alien$phy
+class(alien.phy$phy) #Checking it's multiphylo
+alien.phy$phy
 
 #Load Tree phylum (tree_phylum doc)
 #
 #Run PGLS accounting for phylogenetic uncertain
 tree <- tree_phylm(log(gestaLen) ~ log(adultMass), phy = data$phy, 
-                   data = alien$data, n.tree = 30)
+                   data = alien.phy$data, n.tree = 30)
 # To check summary results:
 summary(tree)
 # Visual diagnostics
@@ -22,4 +28,55 @@ sensi_plot(tree)
 
 View(alien)
 
+
+#2. tree_physig(): tree method for phylogenetic signal
+
+# Load data:
+data(alien)
+# Logtransform data
+alien.data$logMass <- log(alien.data$adultMass) 
+# Run sensitivity analysis:
+tree <- tree_physig(trait.col = "logMass", data = alien.data, phy = alien.phy)
+summary(tree)
+sensi_plot(tree)
+sensi_plot(tree, graphs = 1)
+sensi_plot(tree, graphs = 2)
+
+
+#3. tree_continuous() and tree_discrete: tree method for trait evolution of continous and discrete characters
+
+# Load data:
+primates
+
+# Model trait evolution accounting for phylogenetic uncertainty
+adultMass<-primates$data$adultMass
+names(adultMass)<-rownames(primates$data)
+tree_cont<-tree_continuous(data = adultMass,phy = primates$phy,
+                           model = "OU",n.tree=30,n.cores = 2,track = TRUE)
+# Print summary statistics for the transitions rates, aic-values and (if applicable) 
+# optimisation parameter
+summary(tree_cont)
+## Visual diagnostics
+sensi_plot(tree_cont,graphs="sigsq")
+sensi_plot(tree_cont,graphs="optpar")
+
+
+#Discrete characters 
+# Load data:
+primates
+# Create a binary trait factor 
+adultMass_binary<-ifelse(primates$data$adultMass > 7350, "big", "small")
+adultMass_binary<-as.factor(as.factor(adultMass_binary))
+names(adultMass_binary)<-rownames(primates$data)
+# Model trait evolution accounting for phylogenetic uncertainty
+tree_binary<-tree_discrete(data = adultMass_binary,phy = primates$phy,
+                           model = "ARD",transform = "none",n.tree = 30,n.cores = 2,track = TRUE)
+# Print summary statistics for the transitions rates, aic-values and (if applicable) 
+# optimisation parameter
+summary(tree_binary)
+## Visual diagnostics
+sensi_plot(tree_binary,graphs="q12")
+sensi_plot(tree_binary,graphs="q21")
+
+sensi_plot(tree_binary) 
 
